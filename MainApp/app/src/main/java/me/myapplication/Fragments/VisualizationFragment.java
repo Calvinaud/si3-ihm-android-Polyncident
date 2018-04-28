2f4acf99bbf068aa911dce8cd8436210c1705546
Sebastien Posca <sebastien.posca@etu.unice.fr>
2018-04-28 23:24:16 +0200

    created card_layout_visualization

--- MainApp/app/src/main/java/me/myapplication/Fragments/VisualizationFragment.java
+++ MainApp/app/src/main/java/me/myapplication/Fragments/VisualizationFragment.java
@@ -1,8 +1,52 @@
 package me.myapplication.Fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import me.myapplication.R;
+
 /**
  * Created by user on 28/04/2018.
  */
 
-public class VisualizationFragment {
+public class VisualizationFragment extends android.support.v4.app.Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private GridView gridView;
+
+
+
+    public VisualizationFragment() {
+    }
+
+    public static VisualizationFragment newInstance(int sectionNumber) {
+        VisualizationFragment fragment = new VisualizationFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_visualization, container, false);
+        this.gridView = (GridView) rootView.findViewById(R.id.gridView);
+        return rootView;
+    }
+
 }
--- MainApp/app/src/main/java/me/myapplication/MainActivity.java
+++ MainApp/app/src/main/java/me/myapplication/MainActivity.java
@@ -1,23 +1,23 @@
 package me.myapplication;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
+
+import me.myapplication.Fragments.VisualizationFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -139,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return VisualizationFragment.newInstance(position+1);
         }
 
         @Override
