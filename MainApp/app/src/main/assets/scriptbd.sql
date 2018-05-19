3d1e1731f7b5b72137f73ae15c2935560854c339
Calvin Audier <calvin.audier@etu.unice.fr>
2018-05-19 20:15:22 +0200

    Fix scriptBD

--- MainApp/app/src/main/assets/scriptbd.sql
+++ MainApp/app/src/main/assets/scriptbd.sql
@@ -8,8 +8,21 @@ DROP TABLE IF EXISTS assignations;
 
 --==== Creation of the tables ====
 
+CREATE TABLE users (
+    userId INTEGER PRIMARY KEY,
+    username varchar(30) UNIQUE,
+    password varchar(100),
+    roles VARCHAR(20) NOT NULL CHECK (roles IN ('UTILISATEUR','ADMINISTRATEUR', 'TECHNICIEN'))
+);
+
 CREATE TABLE locations (
     locationId INTEGER PRIMARY KEY,
+    name VARCHAR(30),
+    description TEXT
+);
+
+CREATE TABLE types(
+    typeId INTEGER PRIMARY KEY,
     name VARCHAR(30),
     description TEXT
 );
@@ -18,7 +31,7 @@ CREATE TABLE incidents (
     incidentId INTEGER PRIMARY KEY,
     reporterId INTEGER REFERENCES users,
     locationId INTEGER REFERENCES locations,
-    typeId INTEGER REFERENCES categories,
+    typeId INTEGER REFERENCES types,
     importance INTEGER,
     urlPhoto VARCHAR(100),
     title VARCHAR(30),
@@ -31,32 +44,19 @@ CREATE TABLE followers (
     PRIMARY KEY(incidentId, userId)
 );
 
-CREATE TABLE types(
-    typeId INTEGER PRIMARY KEY,
-    name VARCHAR(30),
-    description TEXT
-);
-
-CREATE TABLE users (
-    userId INTEGER PRIMARY KEY,
-    username varchar(30) UNIQUE,
-    password varchar(100),
-    role VARCHAR(20) NOT NULL CHECK (roles IN ('UTILISATEUR','ADMINISTRATEUR', 'TECHNICIEN'))
-);
-
 CREATE TABLE comments(
     commentId INTEGER PRIMARY KEY,
     userId INTEGER REFERENCES users,
     incidentId INTEGER REFERENCES incidents,
-    date DATETIME,
+    date DATE,
     comment TEXT
 );
 
 CREATE TABLE assignations (
     userId INTEGER REFERENCES users,
     incidentId INTEGER REFERENCES incidents,
-    startDate DATETIME,
-    endDate DATETIME,
+    startDate DATE,
+    endDate DATE,
     comment TEXT,
     PRIMARY KEY(userId, incidentId)
 );
@@ -66,30 +66,30 @@ CREATE TABLE assignations (
 
 --locations
 
-INSERT INTO locations (name) VALUES('E130');
-INSERT INTO locations (name) VALUES('E131');
-INSERT INTO locations (name) VALUES('E132');
+INSERT INTO locations (locationId, name) VALUES('1', 'E130');
+INSERT INTO locations (locationId, name) VALUES('2', 'E131');
+INSERT INTO locations (locationId, name) VALUES('3','E132');
 
-INSERT INTO locations (name) VALUES('O108');
-INSERT INTO locations (name) VALUES('0109');
-INSERT INTO locations (name) VALUES('0110');
+INSERT INTO locations (locationId, name) VALUES('4','O108');
+INSERT INTO locations (locationId, name) VALUES('5','0109');
+INSERT INTO locations (locationId, name) VALUES('6','0110');
 
-INSERT INTO locations (name) VALUES('Parking');
+INSERT INTO locations (locationId, name) VALUES('7','Parking');
 
 --categories
 
-INSERT INTO types (name) VALUES('Fournitures');
-INSERT INTO types (name) VALUES('Matériel cassé');
-INSERT INTO types (name) VALUES('Autres');
+INSERT INTO types (typeid, name) VALUES('1','Fournitures');
+INSERT INTO types (typeid, name) VALUES('2','Matériel cassé');
+INSERT INTO types (typeid, name) VALUES('3','Autres');
 
 --users
-INSERT INTO users (userId, username, password, role) VALUES ("11","Mathieu", "Mathieu", "UTILISATEUR");
-INSERT INTO users (userId, username, password, role) VALUES ("42", "Bob", "Bob", "TECHNICIEN");
+INSERT INTO users (userId, username, password, roles) VALUES (1,'Mathieu', 'Mathieu', 'UTILISATEUR');
+INSERT INTO users (userId, username, password, roles) VALUES (2, 'Bob', 'Bob', 'TECHNICIEN');
 
 --incident
-INSERT INTO incident (incidentId) VALUE ("1");
-INSERT INTO incident (incidentId) VALUE ("2");
+INSERT INTO incidents (incidentId) VALUES (1);
+INSERT INTO incidents (incidentId) VALUES (2);
 
 --assignation
-INSERT INTO assignation (userId, incidentId, startDate, endDate) VALUES ("42", "1", "2018-05-19 15:30:00", "2018-05-20 18:30:00");
-INSERT INTO assignation (userId, incidentId, startDate, endDate) VALUES ("42", "2", "2018-05-19 11:00:00", "2018-05-20 13:00:00");
+INSERT INTO assignations (userId, incidentId, startDate, endDate) VALUES (2, 1, '2018-05-19 15:30:00', '2018-05-20 18:30:00');
+INSERT INTO assignations (userId, incidentId, startDate, endDate) VALUES (2, 2, '2018-05-19 11:00:00', '2018-05-20 13:00:00');
