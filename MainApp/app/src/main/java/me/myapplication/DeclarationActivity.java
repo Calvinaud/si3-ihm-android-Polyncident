4705b9bb181c14a116c5638df33933e4bb5a14dd
Gilles Chandelier <gilles.chandelier@etu.unice.fr>
2018-05-20 22:53:24 +0200

    déclaration apercu cas take photo/video / choisi dans la galerie

--- MainApp/app/src/main/java/me/myapplication/DeclarationActivity.java
+++ MainApp/app/src/main/java/me/myapplication/DeclarationActivity.java
@@ -3,10 +3,16 @@ package me.myapplication;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -15,11 +21,16 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.VideoView;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -39,6 +50,13 @@ public class DeclarationActivity extends Activity {
     private TextView locationLabel;
 
     private View[] collapsibleViews;
+    private final int PICK_IMAGE = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 2;
+    private static final int REQUEST_VIDEO_CAPTURE = 3;
+
+    private String mCurrentPhotoPath;
+
+
 
 
     @Override
@@ -52,6 +70,44 @@ public class DeclarationActivity extends Activity {
 
         fillTypes();
         fillDeclarations();
+        Button btnGalery = findViewById(R.id.btnGalery);
+        btnGalery.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/* video/*");
+                startActivityForResult(intent,PICK_IMAGE);
+
+            }
+        });
+
+        Button btnCapture = findViewById(R.id.btnCapture);
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+
+            }
+        });
+
+        Button btnVideo = findViewById(R.id.btnVideo);
+        btnVideo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+                if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+                }
+
+            }
+        });
     }
 
     private void initElementReferences(){
@@ -76,16 +132,16 @@ public class DeclarationActivity extends Activity {
 
     private void fillTypes(){
         ArrayAdapter<String> types = new ArrayAdapter<String>(getBaseContext(),
-                                                         android.R.layout.simple_spinner_item,
-                                                         IncidentDBHelper.getSingleton().getTypes());
+                android.R.layout.simple_spinner_item,
+                IncidentDBHelper.getSingleton().getTypes());
         types.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         this.typeSpinner.setAdapter(types);
     }
 
     private void fillDeclarations(){
         ArrayAdapter<String> locations = new ArrayAdapter<String>(getBaseContext(),
-                                                             android.R.layout.simple_spinner_item,
-                                                             IncidentDBHelper.getSingleton().getLocations());
+                android.R.layout.simple_spinner_item,
+                IncidentDBHelper.getSingleton().getLocations());
         locations.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         this.locationSpinner.setAdapter(locations);
     }
@@ -114,14 +170,14 @@ public class DeclarationActivity extends Activity {
         public void onClick(View view) {
 
             if(titleEditText.getText().toString().equals("")
-               || descriptionEditText.getText().toString().equals(""))
+                    || descriptionEditText.getText().toString().equals(""))
                 return;
 
             IncidentDBHelper.getSingleton()
                     .insertIncident(0, locationSpinner.getSelectedItemPosition()+1,
-                    typeSpinner.getSelectedItemPosition()+1,importanceSeekBar.getProgress(),
-                    titleEditText.getText().toString(), descriptionEditText.getText().toString()
-            );
+                            typeSpinner.getSelectedItemPosition()+1,importanceSeekBar.getProgress(),
+                            titleEditText.getText().toString(), descriptionEditText.getText().toString()
+                    );
             IncidentDBHelper.getSingleton().logIncidents();
 
             finish();
@@ -144,4 +200,90 @@ public class DeclarationActivity extends Activity {
         }
         return super.dispatchTouchEvent( event );
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView mImageView = findViewById(R.id.imageView4);
+        VideoView mVideoView = findViewById(R.id.videoView);
+
+        if (resultCode != Activity.RESULT_CANCELED) {
+            if (requestCode == PICK_IMAGE) {
+                Uri selectedMediaUri = data.getData();
+                if (selectedMediaUri.toString().contains("image")) {
+                    Log.i("path",selectedMediaUri.toString());
+                    try {
+                        Bitmap bm = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedMediaUri);
+                        mImageView.setImageBitmap(bm);
+                        mImageView.setVisibility(View.VISIBLE);
+                        mVideoView.setVisibility(View.INVISIBLE);
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
+                } else  if (selectedMediaUri.toString().contains("video")) {
+                    mVideoView.setVideoURI(selectedMediaUri);
+                    mImageView.setVisibility(View.INVISIBLE);
+                    mVideoView.setVisibility(View.VISIBLE);
+                }
+
+            }
+
+            else if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                Log.i("uhjk", "ujhnk");
+                mImageView.setImageBitmap(imageBitmap);
+                mImageView.setVisibility(View.VISIBLE);
+                mVideoView.setVisibility(View.INVISIBLE);
+            }
+
+            else if (requestCode == REQUEST_VIDEO_CAPTURE){
+                Uri videoUri = data.getData();
+                mVideoView.setVideoURI(videoUri);
+                mImageView.setVisibility(View.INVISIBLE);
+                mVideoView.setVisibility(View.VISIBLE);
+
+            }
+        }
+    }
+    public Bitmap decodeFile(String path) {
+        try {
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(path, o);
+            // The new size we want to scale to
+            final int REQUIRED_SIZE = 70;
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale = 1;
+            while (o.outWidth / scale / 2 >= REQUIRED_SIZE && o.outHeight / scale / 2 >= REQUIRED_SIZE)
+                scale *= 2;
+
+            // Decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            return BitmapFactory.decodeFile(path, o2);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 }
