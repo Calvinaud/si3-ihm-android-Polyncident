6b562fe04d1b4ab21a0fd1f33c1a231a972f5a35
Calvin Audier <calvin.audier@etu.unice.fr>
2018-05-21 13:32:51 +0200

    Création Package Listener et OnSwipeListener

--- MainApp/app/src/main/java/me/myapplication/Fragments/DayFragment.java
+++ MainApp/app/src/main/java/me/myapplication/Fragments/DayFragment.java
@@ -1,6 +1,7 @@
 package me.myapplication.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -17,7 +18,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import me.myapplication.Adapters.DayRecyclerViewAdapter;
+import me.myapplication.DeclarationActivity;
+import me.myapplication.DisplayDetailsIncidentActivity;
 import me.myapplication.Helpers.IncidentDBHelper;
+import me.myapplication.Listener.OnSwipeTouchListener;
 import me.myapplication.R;
 
 /**
@@ -28,20 +32,17 @@ public class DayFragment extends Fragment {
 
     private static String ARG_SECTION_DATE="date";
 
-    private long date;
+    private Context context;
+    private Date date;
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
     public DayFragment(){}
 
-
     public static DayFragment newInstance(Date currentdate) {
         DayFragment fragment = new DayFragment();
         Bundle args = new Bundle();
-        args.putLong(ARG_SECTION_DATE,currentdate.getTime());
-        Log.i("edea",""+currentdate);
-
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,14 +50,13 @@ public class DayFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i("edea",""+date);
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_planning_day, container, false);
+        final View view = inflater.inflate(R.layout.fragment_planning_day, container, false);
 
         Date currentdate= new Date();
         SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd");
@@ -69,14 +69,22 @@ public class DayFragment extends Fragment {
 
         TextView text= (TextView) view.findViewById(R.id.date);
       //  text.setText(format.format(currentdate));
-        text.setText(""+date);
+        text.setText("truc");
 
         RecyclerView recyclerView = view.findViewById(R.id.incidentList);
         recyclerView.setHasFixedSize(true);
 
-        Context context = view.getContext();
+        context = view.getContext();
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
         recyclerView.setAdapter(new DayRecyclerViewAdapter(context,2, currentdate));
+
+        view.setOnTouchListener(new OnSwipeTouchListener(context) {
+            @Override
+            public void onSwipeLeft() {
+                Intent intent = new Intent(context,DeclarationActivity.class);
+                context.startActivity(intent);
+            }
+        });
 
         return view;
     }
--- MainApp/app/src/main/java/me/myapplication/PlanningActivity.java
+++ MainApp/app/src/main/java/me/myapplication/PlanningActivity.java
@@ -50,7 +50,7 @@ public class PlanningActivity extends AppCompatActivity {
         }
 
         FragmentTransaction ft=getFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment, DayFragment.newInstance(currentdate));
+        ft.replace(R.id.fragment, new DayFragment());
         ft.commit();
 
 //        DayFragment fragment=(DayFragment) getFragmentManager().findFragmentById(R.id.fragment);
