c18e922a6cc119d853f4d6bc88334e9249b6fdb4
Aurélien Zintzmeyer <aurelien.zintzmeyer@etu.unice.fr>
2018-05-21 14:21:02 +0200

    Add a model for the users

---
+++ MainApp/app/src/main/java/me/myapplication/Models/User.java
@@ -0,0 +1,39 @@
+package me.myapplication.Models;
+
+import android.database.Cursor;
+
+public class User {
+
+    private String username;
+    private String emailAddress;
+    private String telephoneNumber;
+    private String role;
+
+    public User(Cursor cursor){
+        if(cursor.getCount() != 1)
+            throw new IllegalArgumentException("The cursor should contains only one row");
+
+        this.username = cursor.getString(cursor.getColumnIndex("username"));
+        this.emailAddress = cursor.getString(cursor.getColumnIndex("emailAddress"));
+        this.telephoneNumber = cursor.getString(cursor.getColumnIndex("telephoneNumber"));
+        this.role = cursor.getString(cursor.getColumnIndex("roles"));
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+}
